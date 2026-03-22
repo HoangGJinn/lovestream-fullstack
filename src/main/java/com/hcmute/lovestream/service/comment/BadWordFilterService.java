bab3db3609359bfa4788c77d4ce880fb1c5b7538
hgdinh2k5 <Hhgadh123@gmail.com>
2026-03-22 20:33:40 +0700

    my change

--- src/main/java/com/hcmute/lovestream/service/comment/BadWordFilterService.java
+++ src/main/java/com/hcmute/lovestream/service/comment/BadWordFilterService.java
@@ -1,8 +1,11 @@
 package com.hcmute.lovestream.service.comment;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class BadWordFilterService {
     private static final List<String> BAD_WORDS = Arrays.asList("chửi_thề", "từ_bậy", "tục_tĩu", "spam_link");
     public boolean containsBadWord(String content) {
