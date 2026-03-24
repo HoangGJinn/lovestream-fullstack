027751b97e5eea2832ccd7f3d2e5898437f46bc6
huy2222 <nguyennhathuy21112005@gmail.com>
2026-03-24 17:00:02 +0700

    fix toc do xem phim

--- src/main/java/com/hcmute/lovestream/controller/web/MediaAssetWebController.java
+++ src/main/java/com/hcmute/lovestream/controller/web/MediaAssetWebController.java
@@ -1,12 +1,15 @@
 package com.hcmute.lovestream.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MediaAssetWebController {
-    @GetMapping("/movie")
-    public String moviePage() {
+    @GetMapping("/watch-movie")
+    public String moviePage(@RequestParam String id, Model model) {
+        model.addAttribute("id", id);
         return "videocontent/watch_movie";
     }
 }
