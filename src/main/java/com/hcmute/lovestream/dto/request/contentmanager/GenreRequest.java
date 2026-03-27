101644ccb3b4e8fb8cc3f7fd834ff82813fda55a
NgQuVi_1410 <vinguyenkg123@gmail.com>
2026-03-27 11:10:46 +0700

    chuyen folder cm - ad

---
+++ src/main/java/com/hcmute/lovestream/dto/request/contentmanager/GenreRequest.java
@@ -0,0 +1,20 @@
+package com.hcmute.lovestream.dto.request.contentmanager;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenreRequest {
+    
+    @NotBlank(message = "Tên thể loại không được để trống")
+    @Pattern(regexp = "^[a-zA-Z0-9\\s\\-ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂÂÊÔƠƯ\\u00C0-\\u024F\\u1E00-\\u1EFF]+$", 
+             message = "Tên thể loại chỉ được chứa chữ cái, số, khoảng trắng và dấu gạch ngang")
+    private String name;
+}
---
+++ src/main/java/com/hcmute/lovestream/service/contentmanager/series/ContentManagerSeriesManagementService.java
@@ -0,0 +1,41 @@
+package com.hcmute.lovestream.service.contentmanager.series;
+
+import com.hcmute.lovestream.dto.request.contentmanager.series.EpisodeUpsertRequest;
+import com.hcmute.lovestream.dto.request.contentmanager.series.SeasonUpsertRequest;
+import com.hcmute.lovestream.dto.request.contentmanager.series.TVSeriesUpsertRequest;
+import com.hcmute.lovestream.entity.Episode;
+import com.hcmute.lovestream.entity.MediaAsset;
+import com.hcmute.lovestream.entity.Season;
+import com.hcmute.lovestream.entity.TVSeries;
+import com.hcmute.lovestream.entity.enums.AssetType;
+import com.hcmute.lovestream.entity.enums.ContentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ContentManagerSeriesManagementService {
+
+    // ---- TV Series ----
+    Page<TVSeries> getSeries(String keyword, ContentStatus status, Pageable pageable);
+    TVSeries getSeriesById(String id);
+    TVSeries createSeries(TVSeriesUpsertRequest request);
+    TVSeries updateSeries(String id, TVSeriesUpsertRequest request);
+    void toggleSeriesStatus(String id);
+    void deleteSeries(String id);
+
+    // ---- Season ----
+    Season getSeasonById(String id);
+    Season createSeason(SeasonUpsertRequest request);
+    Season updateSeason(String id, SeasonUpsertRequest request);
+    void deleteSeason(String id);
+
+    // ---- Episode ----
+    Episode getEpisodeById(String id);
+    Episode createEpisode(EpisodeUpsertRequest request);
+    Episode updateEpisode(String id, EpisodeUpsertRequest request);
+    void deleteEpisode(String id);
+
+    // ---- Assets ----
+    MediaAsset uploadSeriesPoster(String seriesId, org.springframework.web.multipart.MultipartFile file) throws java.io.IOException;
+    MediaAsset addSeriesTrailerFromUrl(String seriesId, String url);
+    MediaAsset addEpisodeVideoFromUrl(String episodeId, String url);
+}
